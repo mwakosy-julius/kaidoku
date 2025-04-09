c0c87abb62d88673bc6d6e3586de29d0abfeeef6
mwakosy-julius <mwakosyajulius619@gmail.com>
2025-04-09 17:49:58 +0300

    removed template

--- app/tools/primer_design/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.primer_design, name='primer_design'),
-]
--- app/tools/quality_control/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-
-class SequenceForm(forms.Form):
-    file = forms.FileField(label="Upload Sequence File")
-    reference_file = forms.FileField(
-        label="Upload File",
-        required=False
-    )
